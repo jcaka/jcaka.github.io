db99d80188758b95ff7235b6d86df9b26c67f243
jcaka <dylanraga+jcaka@gmail.com>
2016-03-20 02:13:41 -0700

    carousel rev2

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 //CAROUSEL
-var activeImg=1;
-var images = document.getElementsByClassName('carousel')[0].getElementsByTagName('img');
-
-console.log(images);
+var activeImg = 1;
+var carouselWrap = document.getElementsByClassName('carousel')[0].getElementsByClassName('wrapper')[0];
+var images = carouselWrap.getElementsByTagName('img');
 
 
 var carousel = setInterval(function(){
-  for( var h=0,len=images.length; h<len; h++){
-    images[h].className = '';
-  }
-  images[activeImg].className = 'active';
-  console.log(activeImg,images.length)
+  carouselWrap.style.transform = 'translate3d(-'+680*activeImg+'px,0,0)';
+  carouselWrap.style.webkitTransform  = 'translate3d(-'+680*activeImg+'px,0,0)';
+  carouselWrap.style.msProperty  = 'translate3d(-'+680*activeImg+'px,0,0)';
+
   if( activeImg >= images.length-1 )
     activeImg = 0;
   else
